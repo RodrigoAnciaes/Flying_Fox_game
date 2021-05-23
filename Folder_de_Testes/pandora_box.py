36d2ec8afc64b0b7851de26816c5fa76920e28ff
RodrigoAnciaes <jogarlolsecundario@gmail.com>
2021-05-23 11:03:59 -0300

    Create pandora_box.py

---
+++ Folder_de_Testes/pandora_box.py
@@ -0,0 +1,27 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+import random
+from config import WIDTH, HEIGHT, INIT, GAME, QUIT
+from init_screen import init_screen
+from game_screen import game_screen
+from end_screen import ending_screen 
+
+pygame.init()
+pygame.mixer.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Flying_Fox')
+
+state = INIT
+while state != QUIT:
+    if state == INIT:
+        state = init_screen(window)
+    elif state == GAME:
+        state = game_screen(window)
+    else:
+        state = ending_screen(window)
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
