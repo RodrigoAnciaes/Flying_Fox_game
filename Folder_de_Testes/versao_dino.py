588569c3607ccbc9f2416b07e0279fae64970a12
RodrigoAnciaes <jogarlolsecundario@gmail.com>
2021-05-27 22:06:23 -0300

    Update versao_dino.py

--- Folder_de_Testes/versao_dino.py
+++ Folder_de_Testes/versao_dino.py
@@ -10,7 +10,6 @@ WIDTH = 880
 HEIGHT = 660
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Flying_Fox')
-gravity = 1
 difficult = 0
 
 count_fox = 0
@@ -35,6 +34,7 @@ class Fox(pygame.sprite.Sprite):
         count_fox = 0
         Fox_WIDTH = 170
         Fox_HEIGHT = 100
+        self.gravity = 1
         Fox1 = pygame.image.load('Folder_de_Testes/assets/img/raposa 1.png').convert_alpha()
         Fox1 = pygame.transform.scale(Fox1, (Fox_WIDTH, Fox_HEIGHT))
         Fox2 = pygame.image.load('Folder_de_Testes/assets/img/raposa2.png').convert_alpha()
@@ -42,11 +42,11 @@ class Fox(pygame.sprite.Sprite):
         Fox3 = pygame.image.load('Folder_de_Testes/assets/img/raposa 3.png').convert_alpha()
         Fox3 = pygame.transform.scale(Fox3, (Fox_WIDTH, Fox_HEIGHT))
         self.flying_one = pygame.image.load('Folder_de_Testes/assets/img/raposafinal.png').convert_alpha()
-        self.flying_one = pygame.transform.scale(self.flying_one, (Fox_WIDTH, Fox_HEIGHT))
+        self.flying_one = pygame.transform.scale(self.flying_one, (100, 100))
 
         self.images = [Fox1,Fox2,Fox3]
 
-        self.count_fox = count_fox
+        self.count_fox = count_fox 
         self.image = Fox1
         
         self.rect = self.image.get_rect()
@@ -62,17 +62,17 @@ class Fox(pygame.sprite.Sprite):
         
         self.rect.y += self.speedy 
 
-        self.speedy += gravity + 0.1 * (-self.speedy)
+        self.speedy += self.gravity + 0.1 * (-self.speedy)
 
         
         self.mask = pygame.mask.from_surface(self.image)
 
         self.count_fox += 1
 
-        print(self.speed_modifier)
+        #print(self.speed_modifier)
 
-        if self.speed_modifier > -30:
-            self.speed_modifier -= 0.003
+        if self.speed_modifier > -12:
+            self.speed_modifier -= 0.0024
 
 
 
@@ -165,6 +165,37 @@ class Coin(pygame.sprite.Sprite):
             self.rect.x = (WIDTH-coin_WIDTH)
             self.rect.y = (HEIGHT - METEOR_HEIGHT)
             self.speedx = random.randint(-5, -3)
+
+
+
+class Predator(pygame.sprite.Sprite):
+    def __init__(self):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        coin_HEIGHT = 50
+        coin_WIDTH = 50
+        self.image = pygame.image.load('Folder_de_Testes/assets/img/piranha.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (coin_WIDTH, coin_HEIGHT))
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.x = (WIDTH-coin_WIDTH)
+        self.rect.y = random.randint(10, 300)
+        self.speedx = random.randint(-5, -3)
+        METEOR_HEIGHT = random.randint(50, 250)
+        
+        
+
+    def update(self):
+        # Atualizando a posição do meteoro
+        METEOR_HEIGHT = random.randint(50, 250)
+        self.rect.x += self.speedx
+        coin_WIDTH = 50
+        # Se o meteoro passar do final da tela, volta para cima e sorteia
+        # novas posições e velocidades
+        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
+            self.rect.x = (WIDTH-coin_WIDTH)
+            self.rect.y = (HEIGHT - METEOR_HEIGHT)
+            self.speedx = random.randint(-5, -3)
             
 score = 0
 score_coin = 0
@@ -177,6 +208,7 @@ FPS = 30
 all_sprites = pygame.sprite.Group()
 all_meteors = pygame.sprite.Group()
 all_coins =   pygame.sprite.Group()
+all_predators = pygame.sprite.Group()
 # Criando o jogador
 player = Fox()
 all_sprites.add(player)
@@ -190,9 +222,32 @@ coin = Coin()
 all_coins.add(coin)
 all_sprites.add(coin)
 
+
+predator = Predator()
+all_predators.add(predator)
+all_sprites.add(predator)
+added = True
+
 # ===== Loop principal =====
 while game:
+
+
     fpdif = FPS + difficult
+  
+    
+
+    if fpdif > 40 and added == True:
+        print(added)
+        
+        predator = Predator()
+        all_predators.add(predator)
+        all_sprites.add(predator)
+        added = False
+
+
+
+
+    
     #print(fpdif)
     clock.tick(fpdif)
     #print(clock)
@@ -225,6 +280,11 @@ while game:
     all_sprites.update()
     
     hits = pygame.sprite.spritecollide(player,all_meteors,True, pygame.sprite.collide_mask)
+    if len(hits) > 0:
+        game = False
+    hits = pygame.sprite.spritecollide(player,all_predators,True, pygame.sprite.collide_mask)
+    if len(hits) > 0:
+        game = False
     
     colect_coin = pygame.sprite.spritecollide(player,all_coins,True, pygame.sprite.collide_mask)
 
