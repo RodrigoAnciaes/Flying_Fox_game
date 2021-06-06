60c9793c33a58bdeba295b66f2d572139a89b868
RodrigoAnciaes <jogarlolsecundario@gmail.com>
2021-06-06 15:36:28 -0300

    Create versao_final.py

---
+++ Flying_Fox_Game/versao_final.py
@@ -0,0 +1,32 @@
+
+
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+import random
+from config import WIDTH, HEIGHT, INIT, GAME, QUIT
+from first_screen import init_screen
+from instructions_screen import instruc_screen
+from game_screen import gaming_screen
+from death_screen import end_screen
+
+pygame.init()
+pygame.mixer.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Navinha')
+
+state = INIT
+while state != QUIT:
+    if state == INIT:
+        state = init_screen(window)
+    elif state == TUTO:
+        state = instruc_screen(window)
+    elif state == GAME:
+        state = gaming_screen(window)
+    else:
+        state = end_screen(window)
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
