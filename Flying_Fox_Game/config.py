ece29dda60ec1b6359f56da93d0e1c442a3de8b0
RodrigoAnciaes <jogarlolsecundario@gmail.com>
2021-06-07 17:40:31 -0300

    Update config.py

--- Flying_Fox_Game/config.py
+++ Flying_Fox_Game/config.py
@@ -1,11 +1,11 @@
 from os import path
 from random import *
 
-
+# Define as variáveis utilizadas na tranzição de telas
 INIT = 0
 GAME = 1
 DONE = 2
 QUIT = 3
-
+# Define a altura e espessura da tela (acabaram por ser redefinidas também no resto dos códigos, defido a problemas com a ordem dos arquivos)
 WIDTH = 880
 HEIGHT = 660
