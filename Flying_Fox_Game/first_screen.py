97d429c5ffa932f343248aa74c91728c05edef9b
Enzo Cunha <81170064+enzosvc@users.noreply.github.com>
2021-06-07 10:48:59 -0300

    Update first_screen.py

--- Flying_Fox_Game/first_screen.py
+++ Flying_Fox_Game/first_screen.py
@@ -1,10 +1,8 @@
-########o codigo abaixo é o do handout,,,, esta apenas para nos basearmos##########
-
 import pygame
 import random
 from os import path
 
-from config import IMG_DIR, BLACK, FPS, GAME, QUIT
+from config import GAME, QUIT
 
 
 def init_screen(screen):
@@ -12,14 +10,14 @@ def init_screen(screen):
     clock = pygame.time.Clock()
 
     # Carrega o fundo da tela inicial
-    background = pygame.image.load(path.join(IMG_DIR, 'inicio.png')).convert()
+    background = pygame.image.load('Flying_Fox_Game/assets/img/tela de inicio final.png').convert()
     background_rect = background.get_rect()
 
     running = True
     while running:
 
         # Ajusta a velocidade do jogo.
-        clock.tick(FPS)
+        clock.tick(60)
 
         # Processa os eventos (mouse, teclado, botão, etc).
         for event in pygame.event.get():
@@ -33,7 +31,7 @@ def init_screen(screen):
                 running = False
 
         # A cada loop, redesenha o fundo e os sprites
-        screen.fill(BLACK)
+        #screen.fill(BLACK)
         screen.blit(background, background_rect)
 
         # Depois de desenhar tudo, inverte o display.
