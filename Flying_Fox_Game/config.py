3756eb6771ed0df2d4942bb0d8756423c5b734e2
RodrigoAnciaes <jogarlolsecundario@gmail.com>
2021-06-07 10:30:53 -0300

    Update config.py

--- Flying_Fox_Game/config.py
+++ Flying_Fox_Game/config.py
@@ -1,30 +1,8 @@
 from os import path
 from random import *
-# Estabelece a pasta que contem as figuras e sons.
-IMG_DIR = path.join(path.dirname(__file__), 'assets', 'img')
-SND_DIR = path.join(path.dirname(__file__), 'assets', 'sounds')
-FNT_DIR = path.join(path.dirname(__file__), 'assets', 'font')
 
-# Dados gerais do jogo.
-WIDTH = 880 # Largura da tela
-HEIGHT = 600 # Altura da tela
-FPS = 60 # Frames por segundo
 
-# Define tamanhos
-Tronco_WIDTH = 50
-Tronco_HEIGHT = 88
-Fox_WIDTH = 50
-Fox_HEIGHT = 38
-
-# Define algumas variáveis com as cores básicas
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-
-# Estados para controle do fluxo da aplicação
 INIT = 0
 GAME = 1
-QUIT = 2
+DONE = 2
+QUIT = 3
