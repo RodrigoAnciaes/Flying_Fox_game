8288af6dc6268a9513016d2ea0bdfb0cab320dd9
Enzo Cunha <81170064+enzosvc@users.noreply.github.com>
2021-06-10 00:00:39 -0300

    Update scoreboard__.py

--- Folder_de_Testes/scoreboard__.py
+++ Folder_de_Testes/scoreboard__.py
@@ -1,1 +1,4 @@
 #tentando criar uma tabela de valores que ficam guardados como os "recordes"
+
+
+#Ate tentamos progredir com essa ideia, mas todos os codigos testados no VS ate aqui nao deram muito certo e nao temos mais tempo para tentar criar um socoreboard funcional
